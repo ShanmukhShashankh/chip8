ff363a7691b8762a3111e4a0d82c7e8d817f7a5c
ShanmukhShashankh <shanmukhshashankh@proton.me>
2025-11-29 07:20:29 +0530

    Shifting Funcationality to core chip8 to it's constituent files

---
+++ include/chip8.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <types.h>
+
+class Chip8 {
+  public:
+    Chip8();
+    void CPUReset(); 
+    void cycle();
+    void updateTimers();
+
+    BYTE keypad[16];
+    BYTE screenData[32][64];
+    bool drawFlag;
+
+  private:
+    void decode(WORD opcode);
+
+    // Memory and Registers
+    BYTE gameMemory[0x1000];
+    BYTE registers[16];
+    WORD addressI;
+    WORD programCounter;
+    WORD stack[16];
+    BYTE stackPointer;
+
+    // Timers
+    BYTE delayTimer;
+    BYTE soundTimer;
+
+    // Font Data
+    static const BYTE fontset[80];
+};
